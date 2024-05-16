b98e9a71008b38202d3ffb65bd97e85df42e1f92
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2024-05-16 11:55:55 -1200

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="justify-center items-center flex h-screen text-center">
+      <p className='text-5xl animate-bounce'>WE ARE UNDER <b className='text-red-900'>CONSTRUCTION</b></p>
     </div>
   );
 }
